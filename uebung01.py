fe2f03cd0bd03ba489cc63b9a68faf23250c82cc
lagl1427 <74137846+lagl1427@users.noreply.github.com>
2020-11-09 23:12:08 +0100

    Add files via upload

--- uebung01.py
+++ uebung01.py
@@ -1,7 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 name = "whDatadat.sec"
 data = np.loadtxt(name, dtype=np.object, comments='#', delimiter=None)
 # Um die w und h Daten aus dem array data auszulesen, k¨onnen Sie so vorgehen
 w = data[:,0].astype(float)
 h = data[:,1].astype(float)
+
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
